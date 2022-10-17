512adb2f2b82a735b817871b7095af0a3e75fe0d
Amauri <hamauri@aol.com>
2022-10-17 15:11:32 -0700

    first draft complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,28 @@
 var timerBlocksEl = document.querySelectorAll('.time-block');
 
 var DateTime = luxon.DateTime;
-var taskList = ['testing1', 'testing2', 'testing3', 'testing4', 'testing5', 'testing6', 'testing7', 'testing8', 'testing9']; //emtpy out later
-
-localStorage.setItem('tasks', JSON.stringify(taskList)); // delete later
+var taskList = []; //emtpy out later
 
 function onLoad() {
-    var previousDate;
-    var currentDate = DateTime.now().toLocaleString();
+    var saveBtnEl = document.querySelectorAll('.saveBtn');
     var currentDayEl = document.querySelector('#currentDay');
 
     currentDayEl.textContent = DateTime.now().toFormat('DDDD');
 
-    //load local data if same day
-    previousDate = localStorage.getItem('date');
-    
+    for(var i = 0; i < saveBtnEl.length; i++){
+        saveBtnEl[i].addEventListener('click', saveTask);
+    }
+
+    setWorkTimes();
+    loadLocalData();
+    fillTimeBlocks();
+}
+
+//load local data if same day
+function loadLocalData() {
+    var previousDate = localStorage.getItem('date');
+    var currentDate = DateTime.now().toLocaleString();
+
     if(currentDate == previousDate) {
         taskList = JSON.parse(localStorage.getItem('tasks'));
     }else{
@@ -24,14 +32,51 @@ function onLoad() {
     localStorage.setItem('date', currentDate);
 }
 
-function createTimeBlocks() {
+function setWorkTimes() {
+    var hoursEl = document.querySelectorAll('.hour p');
+
+    for(var i = 0; i < hoursEl.length; i++){
+        var hour = DateTime.fromObject({hour: i + 9})
+        
+        hoursEl[i].textContent = hour.toLocaleString(DateTime.TIME_SIMPLE);
+
+        setColorClass(i, hour.toFormat('HH'))
+    }
+}
+
+//sets hours for blocks depending on time
+function setColorClass(index, hour) {
+    var timeBlockEl= document.getElementById((index + 9) * 100);
+    var currentHour = DateTime.now().toFormat('HH');
+
+    if(hour < currentHour) {
+        timeBlockEl.classList.add('past')
+        //console.log(hour + " is earlier than " + currentHour);
+    }else if (hour == currentHour) {
+        timeBlockEl.classList.add('present')
+        //console.log(hour + " is the same as " + currentHour);
+    }else {
+        timeBlockEl.classList.add('future')
+        //console.log(hour + " is later than " + currentHour);
+    }
+}
+
+function saveTask(event) {
+    //selects grand-parent div then selects desc inside it
+    currentDescEl = event.composedPath()[2].querySelector('.description');
+    //uses id to find index in task list
+    index = (event.composedPath()[2].id / 100) - 9;
+    taskList[index] = currentDescEl.value
+
+    localStorage.setItem('tasks', JSON.stringify(taskList));
+}
+
+function fillTimeBlocks() {
     taskEl = document.querySelectorAll('.description');
 
-    // 0900-1700 military time
-    for(var i = 0; i < 9; i++) {
-        taskEl[i].textContent = taskList[i];
+    for(var i = 0; i < taskEl.length; i++) {
+        taskEl[i].value = taskList[i];
     } 
 }
 
 onLoad();
-createTimeBlocks();
